adf1f34164016abb70434b70543dc277d7772c26
Shahaf Shumel <shahaflidor@gmail.com>
2020-07-16 22:29:44 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <OutDoorHeader></OutDoorHeader>
+    <h1>OutDoor!</h1>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header";
+export default {
+  components: {
+    OutDoorHeader: Header
+  }
+};
+</script>
+
+<style lang="scss"></style>
