f92d6d4b2a20d6112c2248f2f30aa62b289204fe
Shahaf Shumel <shahaflidor@gmail.com>
2020-07-20 11:58:30 +0300

    roomview data

--- src/components/rooms/RoomView.vue
+++ src/components/rooms/RoomView.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
-    <h1>RoomView page</h1>
+    <h1>{{ currentRoom.title }}</h1>
+    <h2>{{ currentRoom.description }}</h2>
+    <h2>Participants: {{ currentRoom.participants }}</h2>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["id", "title", "desc", "participants"]
+  data() {
+    return {
+      id: this.$route.params.id,
+      rooms: []
+    };
+  },
+  created() {
+    this.rooms = this.$store.getters.getRooms;
+    // console.log(this.rooms);
+  },
+  computed: {
+    currentRoom() {
+      return this.rooms.find(room => room.id == this.id);
+    }
+  }
 };
 </script>
 
--- src/data/rooms.js
+++ src/data/rooms.js
@@ -1,24 +1,24 @@
 export default [
   {
-    id: 1,
+    id: 1827,
     title: "Basketball Game",
     description: "2x2/3x3 over the age of 20",
     participants: 4
   },
   {
-    id: 2,
+    id: 9283,
     title: "Evening Yoga",
-    description: "One hour of Shakti Yogo. experienced only",
+    description: "One hour of Shakti Yoga. experienced only",
     participants: 3
   },
   {
-    id: 3,
+    id: 4303,
     title: "Weed Hangout",
     description: "Just chilling & smoking at my basment",
     participants: 6
   },
   {
-    id: 4,
+    id: 5893,
     title: "Football Game",
     description: "5x5 game, over the age of 25, one hour game",
     participants: 10
