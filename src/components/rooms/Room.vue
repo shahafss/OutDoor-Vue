0cd358905578fe0ac2955a696b564234fafe74ac
Shahaf Shumel <shahaflidor@gmail.com>
2020-08-09 20:01:43 +0300

    disable room when full fixes #5

--- src/components/rooms/Room.vue
+++ src/components/rooms/Room.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="col-sm-6 col-md-4">
-    <div class="panel panel-success">
+    <router-link
+      :to="'/room/' + room.id"
+      tag="div"
+      :disabled="isFull && !isJoinedUser"
+      :event="!isFull || isJoinedUser ? 'click' : ''"
+      class="panel panel-success"
+    >
       <div class="panel-heading">
         <h3 class="panel-title">{{ room.title }}</h3>
       </div>
@@ -13,22 +19,37 @@
         </div>
         <div class="pull-right">
           <router-link
+            v-if="!isFull"
             :to="'/room/' + room.id"
             class="btn btn-success"
             tag="button"
           >
             Join
           </router-link>
+          <button v-else class="btn btn-default" disabled>Full</button>
         </div>
       </div>
-    </div>
+    </router-link>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["room"]
+  props: ["room"],
+  computed: {
+    isFull() {
+      return this.room.participants == this.room.joinedUsers.length;
+    },
+    isJoinedUser() {
+      if (this.$store.getters.getUser)
+        return this.room.joinedUsers.includes(this.$store.getters.getUser.id);
+    }
+  }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.panel {
+  cursor: pointer;
+}
+</style>
